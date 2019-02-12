a41d553f447f9b9f9ce0cd6ec23849b64c442abc
Tom Dolphin <thedolphingroup1@gmail.com>
2019-02-12 04:20:32 -0700

    print off day of week using switch statement

--- DayOfWeekChallenge/src/com/tomdolphin/Main.java
+++ DayOfWeekChallenge/src/com/tomdolphin/Main.java
@@ -3,41 +3,44 @@ package com.tomdolphin;
 public class Main {
 
     public static void main(String[] args) {
+        printDayOfTheWeek(4);
+    }
 
-            int dayOfWeek = 9;
+    private static void printDayOfTheWeek(int day) {
 
-            switch(dayOfWeek) {
-                case 1:
+        switch (day) {
+            case 1:
                 System.out.println("Monday");
                 break;
 
-                case 2:
+            case 2:
                 System.out.println("Tuesday");
                 break;
 
-                case 3:
+            case 3:
                 System.out.println("Wednesday");
                 break;
 
-                case 4:
+            case 4:
                 System.out.println("Thursday");
                 break;
 
-                case 5:
+            case 5:
                 System.out.println("Friday");
                 break;
 
-                case 6:
+            case 6:
                 System.out.println("Saturday");
                 break;
 
-                case 7:
+            case 7:
                 System.out.println("Sunday");
                 break;
 
-                default:
-                    System.out.println("Invalid Day");
-                    break;
-            }
+            default:
+                System.out.println("Invalid Day");
+                break;
+        }
     }
 }
+
---
+++ NumberInWord/src/com/tomdolphin/Main.java
@@ -0,0 +1,60 @@
+package com.tomdolphin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        printNumberInWord(-1);
+        printNumberInWord(0);
+        printNumberInWord(1);
+        printNumberInWord(2);
+        printNumberInWord(3);
+        printNumberInWord(4);
+        printNumberInWord(5);
+        printNumberInWord(6);
+        printNumberInWord(7);
+        printNumberInWord(8);
+        printNumberInWord(9);
+        printNumberInWord(10);
+    }
+
+    public static void printNumberInWord(int number) {
+        switch (number){
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            default:
+                System.out.println("Other");
+                break;
+
+
+        }
+    }
+}
